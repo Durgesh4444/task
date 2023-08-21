611a4b731bd99a2108425465c75cf6347ddcb05b
Durgesh_Sonkusare <7276104188d@gmail.com>
2023-08-21 12:05:42 +0530

    third commit

--- src/component/Right.jsx
+++ src/component/Right.jsx
@@ -4,24 +4,28 @@ import Main from './Main'
 import New from './New'
 
 
+
 const Right = (prop) => {
   const [change, setChange]=useState("createminus")
 
+  const [adjust, setAdjust]=useState("")
   const handlechange3 =()=>{
     setChange("createplus")
-    prop.setSilent("silent2")
+    prop.setSilent("silent")
     prop.setSilent2("silent2")
+    setAdjust("adjust")
   }
 
   const handlechange4 =()=>{
     setChange("createminus")
     prop.setSilent("")
     prop.setSilent2("")
+    setAdjust("")
   }
 
   return (
     
-    <div className={'right '+prop.silent}>
+    <div className={'right '+prop.silent + " " + adjust}>
         <Header change1={prop.change1} silent={prop.silent}/>
         <Main value={handlechange3} scroll={prop.scroll} silent={prop.silent}/>
         <New value={change} value2={handlechange4}/>
